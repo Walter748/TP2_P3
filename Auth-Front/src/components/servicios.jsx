043e5daf1deb5529363e355658665ed24f08d609
walter748 <135364413+Walter748@users.noreply.github.com>
2024-06-24 12:38:07 -0300

    Front

--- Auth-Front/src/components/servicios.jsx
+++ Auth-Front/src/components/servicios.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../style/servicios.css'
 
 const Servicios = ({ servicios }) => {
     return (
         <div>
             <h2>Servicios Ofrecidos</h2>
-            <ul>
+            <ul className='servicios'>
                 {servicios.map((servicio, index) => (
                     <li key={index}>{servicio.nombre}</li>
                 ))}
