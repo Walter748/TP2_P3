354b1e37fd980b43aae1a1e450a39c01193ce7ba
walter748 <135364413+Walter748@users.noreply.github.com>
2024-06-25 20:29:30 -0300

    Front

--- Auth-Front/src/components/perfiles.jsx
+++ Auth-Front/src/components/perfiles.jsx
@@ -14,31 +14,31 @@ function Perfiles() {
                 <h2>Walter</h2>
                 <p>Desarrollador Frontend</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque repellendus laudantium numquam aspernatur quasi minus, praesentium voluptatem ratione consectetur et omnis maiores ea odit, culpa ipsam minima aliquam sequi!</p>
+                <Link to='/portafolio' className='botonPer'>Mas info</Link>
             </article>
             <article>
                 <h2>Walter</h2>
                 <p>Desarrollador Frontend</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque repellendus laudantium numquam aspernatur quasi minus, praesentium voluptatem ratione consectetur et omnis maiores ea odit, culpa ipsam minima aliquam sequi!</p>
+                <Link to='/portafolio' className='botonPer'>Mas info</Link>
             </article>
             <article>
                 <h2>Walter</h2>
                 <p>Desarrollador Frontend</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque repellendus laudantium numquam aspernatur quasi minus, praesentium voluptatem ratione consectetur et omnis maiores ea odit, culpa ipsam minima aliquam sequi!</p>
+                <Link to='/portafolio' className='botonPer'>Mas info</Link>
             </article>
             <article>
                 <h2>Walter</h2>
                 <p>Desarrollador Frontend</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque repellendus laudantium numquam aspernatur quasi minus, praesentium voluptatem ratione consectetur et omnis maiores ea odit, culpa ipsam minima aliquam sequi!</p>
+                <Link to='/portafolio' className='botonPer'>Mas info</Link>
             </article>
             <article>
                 <h2>Walter</h2>
                 <p>Desarrollador Frontend</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque repellendus laudantium numquam aspernatur quasi minus, praesentium voluptatem ratione consectetur et omnis maiores ea odit, culpa ipsam minima aliquam sequi!</p>
-            </article>
-            <article>
-                <h2>Walter</h2>
-                <p>Desarrollador Frontend</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque repellendus laudantium numquam aspernatur quasi minus, praesentium voluptatem ratione consectetur et omnis maiores ea odit, culpa ipsam minima aliquam sequi!</p>
+                <Link to='/portafolio' className='botonPer'>Mas info</Link>
             </article>
         </main>
         
